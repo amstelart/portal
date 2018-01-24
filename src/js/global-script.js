723727dfb1884d3180c1543945f23a5027736c05
Артем <amstelart@MacBook-Pro-Artem.local>
2018-01-24 05:12:04 +0300

    правки

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,24 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+
+  // Открытие/закрытие блока с кодом
+  $('.tags__toggler').on('click', function (e) {
+    var $toggler = $(this),
+        $tagsBody = $toggler.closest('.tags').find('.tags__body'),
+        shownClass = 'tags__body--shown',
+        textOpen = $toggler.data('text-open'),
+        textClose = $toggler.data('text-close');
+
+    if ($tagsBody.hasClass(shownClass)) {
+      $tagsBody.removeClass(shownClass);
+      $toggler.text(textOpen);
+    } else {
+      $tagsBody.addClass(shownClass);
+      $toggler.text(textClose);
+    }
+  });
+
+});
 
 // Изоляция без jQuery
 // (function(){
